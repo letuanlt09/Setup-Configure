ad42d92473a3f084638e8388ef8902c471ad1938
Tuan Le <letuanlt09@gmail.com>
2019-09-06 08:40:07 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,67 @@
+var http = require('http'), 
+    fs = require('fs'), 
+    url = require('url'),
+    port = 8080;
+
+/* Global variables */
+var listingData, server;
+var requestHandler = function(request, response) {
+  var parsedUrl = url.parse(request.url);
+  if(parsedUrl.pathname == "/listings"){
+    response.writeHead(200,{"Content-Type":"application/json"});
+    //response.write("The list:\n");
+    response.write(listingData);
+    //response.write("-------------------------");
+    //response.write("\nEND LIST\n");
+  }
+  else {
+    response.writeHead(404,{"Content-Type":"text/plain"});
+    response.write("Bad gateway error");
+  }
+  response.end();
+
+  /*
+    Your request handler should send listingData in the JSON format as a response if a GET request 
+    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+
+    HINT: Explore the request object and its properties 
+    HINT: Explore the response object and its properties
+    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+    
+    HINT: Explore how callback's work 
+    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+    
+    HINT: Explore the list of MIME Types
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+   */
+};
+
+
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  
+  /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+
+    HINT: Check out this resource on fs.readFile
+    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+
+    HINT: Read up on JSON parsing Node.js
+   */
+
+    //Check for errors
+  if(err){
+    console.log("Read file error");
+  }
+  else{
+   //Save the sate in the listingData variable already defined
+    listingData = data;
+   //Creates the server
+   //Start the server
+    server = http.createServer(requestHandler);
+    server.listen(port,function(){
+      console.log("Server is listening on: http://localhost:"+port);
+    });
+  }
+});
